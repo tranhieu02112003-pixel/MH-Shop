6bbc5e244757c9519a97356cceef394a1f801b88
tranhieu02112003-pixel <tranhieu02112003@gmail.com>
2025-12-03 21:27:09 +0700

    Update product-data.js

--- product-data.js
+++ product-data.js
@@ -3,6 +3,6 @@ const productList = [
   { id: 1, name: "Terrarium Mica 20cm", price: "120.000đ", img: "https://via.placeholder.com/600x400?text=Terrarium+20cm", description: "Bể mica 20cm - đẹp để bàn, tặng cây nhỏ." },
   { id: 2, name: "Terrarium Mini 10cm", price: "65.000đ", img: "https://via.placeholder.com/600x400?text=Terrarium+10cm", description: "Mẫu mini gọn nhẹ, phù hợp làm quà." },
   { id: 3, name: "Phụ kiện trang trí", price: "25.000đ", img: "https://via.placeholder.com/600x400?text=Accessory", description: "Đá, cây nhỏ, đồ trang trí." },
-  { id: 3, name: "Phụ kiện trang trí 1", price: "55.000đ", img: "https://via.placeholder.com/600x400?text=Accessory", description: "Đồ trang trí." },
-  { id: 3, name: "Phụ kiện trang trí 2", price: "35.000đ", img: "https://via.placeholder.com/600x400?text=Accessory", description: "Đá, cây nhỏ, đồ trang trí." }
+  { id: 4, name: "Phụ kiện trang trí 1", price: "55.000đ", img: "https://via.placeholder.com/600x400?text=Accessory", description: "Đồ trang trí." },
+  { id: 5, name: "Phụ kiện trang trí 2", price: "35.000đ", img: "https://via.placeholder.com/600x400?text=Accessory", description: "Đá, cây nhỏ, đồ trang trí." }
 ];
